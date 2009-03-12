246c81cc0c96f198f5ecd762557efe815234e719
kgalinsk <kgalinsk@061605af-e407-0410-a945-9c8a22772c06>
2009-03-12 19:50:21 +0000

    Replaced $$hashref{} with $hashref->. Using version.

--- trunk/lib/JCVI/Translator/Utils.pm
+++ trunk/lib/JCVI/Translator/Utils.pm
@@ -41,7 +41,8 @@ use base JCVI::Translator;
 use strict;
 use warnings;
 
-our $VERSION = '0.2.2';
+use version;
+our $VERSION = qv('0.2.3');
 
 use Log::Log4perl qw(:easy);
 use Params::Validate qw(:all);
@@ -373,7 +374,7 @@ sub regex {
           (
             $type eq 'start'
             ? keys %{ $self->{"${prefix}starts"} }
-            : @{ $$self{"${prefix}reverse"}{'*'} }
+            : @{ $self->{"${prefix}reverse"}{'*'} }
           );
         $self->{"${prefix}${type}Regex"} = qr/$regex/;
     }
