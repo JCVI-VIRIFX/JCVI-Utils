41bc7b12da262eecc0fac636240360fc03c01a34
kgalinsk <kgalinsk@061605af-e407-0410-a945-9c8a22772c06>
2009-03-18 17:49:56 +0000

    Committing version 0.1.5

--- trunk/lib/JCVI/AATools.pm
+++ trunk/lib/JCVI/AATools.pm
@@ -1,4 +1,4 @@
-# AATools
+# JCVI::AATools
 #
 # $Author: kgalinsk $
 # $Date: 2008-11-14 10:35:59 -0500 (Fri, 14 Nov 2008) $
@@ -18,10 +18,6 @@ JCVI::AATools - JCVI Basic Amino Acid tools
 Provides a set of functions and predefined variables which
 are handy when working with Amino Acids.
 
-=head1 AUTHOR
-
-Kevin Galinsky, <kgalinsk@jcvi.org>
-
 =cut
 
 package JCVI::AATools;
@@ -29,38 +25,37 @@ package JCVI::AATools;
 use strict;
 use warnings;
 
-our $VERSION = '0.1.0';
+use version;
+our $VERSION = qv('0.1.5');
 
 use Exporter 'import';
+
+our @EXPORT_OK = qw(
+  %ambiguousForward
+  %ambiguousMap
+  %aaAbbrev
+
+  $aas
+  $aaMatch
+  $aaFail
+  $strictAAs
+  $strictMatch
+  $strictFail
+  $ambigs
+  $ambigMatch
+  $ambigFail
+);
+
 our %EXPORT_TAGS = (
-    all => [
-        qw(%ambiguousForward
-            %ambiguousMap
-            %aaAbbrev
-
-            $aas
-            $aaMatch
-            $aaFail
-            $strictAAs
-            $strictMatch
-            $strictFail
-            $ambigs
-            $ambigMatch
-            $ambigFail)
-    ],
-
+    all   => \@EXPORT_OK,
     funcs => [qw()]
 );
 
-our @EXPORT_OK = @{ $EXPORT_TAGS{all} };
-
 =head1 VARIABLES
-
-=over
 
 =cut
 
-=item Ambiguous Mappings
+=head2 AMBIGUOUS MAPPINGS
 
 Two ambiguous mapping hashes. One maps from the amino acid
 forward to the possible ambiguous amino acid, and one is a
@@ -68,23 +63,25 @@ map of what each ambiguous amino acid means.
 
 =cut
 
-our %ambiguousForward = ( A => 'B',
-                          B => 'B',
-                          D => 'B',
-                          I => 'J',
-                          J => 'J',
-                          L => 'J',
-                          E => 'Z',
-                          Q => 'Z',
-                          Z => 'Z'
+our %ambiguousForward = (
+    A => 'B',
+    B => 'B',
+    D => 'B',
+    I => 'J',
+    J => 'J',
+    L => 'J',
+    E => 'Z',
+    Q => 'Z',
+    Z => 'Z'
 );
 
-our %ambiguousMap = ( B => [ 'A', 'D' ],
-                      J => [ 'I', 'L' ],
-                      Z => [ 'E', 'Q' ]
+our %ambiguousMap = (
+    B => [ 'A', 'D' ],
+    J => [ 'I', 'L' ],
+    Z => [ 'E', 'Q' ]
 );
 
-=item %aaAbrev
+=head2 %aaAbrev
 
 Hash from one letter code for amino acids to the three
 letter abbreviations. Includes ambiguous amino acids as well
@@ -92,35 +89,36 @@ as selenocysteine and pyrrolysine.
 
 =cut
 
-our %aaAbbrev = ( A => 'Ala',
-                  B => 'Asx',
-                  C => 'Cys',
-                  D => 'Asp',
-                  E => 'Glu',
-                  F => 'Phe',
-                  G => 'Gly',
-                  H => 'His',
-                  I => 'Ile',
-                  J => 'Xle',
-                  K => 'Lys',
-                  L => 'Leu',
-                  M => 'Met',
-                  N => 'Asn',
-                  O => 'Pyl',
-                  P => 'Pro',
-                  Q => 'Gln',
-                  R => 'Arg',
-                  S => 'Ser',
-                  T => 'Thr',
-                  U => 'Sec',
-                  V => 'Val',
-                  W => 'Trp',
-                  X => 'Xaa',
-                  Y => 'Tyr',
-                  Z => 'Glx'
+our %aaAbbrev = (
+    A => 'Ala',
+    B => 'Asx',
+    C => 'Cys',
+    D => 'Asp',
+    E => 'Glu',
+    F => 'Phe',
+    G => 'Gly',
+    H => 'His',
+    I => 'Ile',
+    J => 'Xle',
+    K => 'Lys',
+    L => 'Leu',
+    M => 'Met',
+    N => 'Asn',
+    O => 'Pyl',
+    P => 'Pro',
+    Q => 'Gln',
+    R => 'Arg',
+    S => 'Ser',
+    T => 'Thr',
+    U => 'Sec',
+    V => 'Val',
+    W => 'Trp',
+    X => 'Xaa',
+    Y => 'Tyr',
+    Z => 'Glx'
 );
 
-=item Basic Variables
+=head2 BASIC VARIABLES
 
 Basic useful amino acid variables. A list of valid
 characters for amino acids, a stricter list containing just
@@ -144,6 +142,8 @@ our $ambigFail  = qr/[^$ambigs]/i;
 
 1;
 
-=back
+=head1 AUTHOR
+
+Kevin Galinsky, <kgalinsk@jcvi.org>
 
 =cut
