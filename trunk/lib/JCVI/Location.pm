9bb4c099d5f921ad4f60a83c4cda1aad5250575a
kgalinsk <kgalinsk@061605af-e407-0410-a945-9c8a22772c06>
2009-10-22 23:16:57 +0000

    JCVI::Location now contains a JCVI::Range object and you are able to access it's methods directly.

--- trunk/lib/JCVI/Location.pm
+++ trunk/lib/JCVI/Location.pm
@@ -16,11 +16,8 @@ package JCVI::Location;
 use warnings;
 use strict;
 
-use base qw( JCVI::Range::Interface );
-
 use Carp;
-use List::Util qw( min max );
-use Params::Validate;
+use JCVI::Range;
 
 =head1 NAME
 
@@ -28,11 +25,11 @@ JCVI::Location - locations on DNA sequences
 
 =head1 VERSION
 
-Version 0.0.1
+Version 0.0.2
 
 =cut
 
-use version; our $VERSION = qv('0.0.1');
+use version; our $VERSION = qv('0.0.2');
 
 =head1 SYNOPSIS
 
@@ -42,190 +39,141 @@ the case of a partial location.
 
     use JCVI::Location;
 
-    my $location = JCVI::Location->new_53( $source, $end5, $end3 );
-    my $location = JCVI::Location->new_53( $source, $end5, $end3, $phase );
+    # See JCVI::Range for a list of constructors
+    my $location = JCVI::Location->$new_function( $source, \@params );
+    my $location = JCVI::Location->$new_function( $source, \@params, $phase );
+
+    my $location = JCVI::Location->new_53( $source, [ $end5, $end3 ] );
+    my $location = JCVI::Location->new_53( $source, [ $end5, $end3 ], $phase );
 
     # List of getters/setters
     my $source = $location->source();
+    my $range  = $location->range();
+    my $phase  = $location->phase();
+    
+    # Getters/setters from JCVI::Range
     my $lower  = $location->lower();
     my $upper  = $location->upper();
     my $strand = $location->strand();
     my $phase  = $location->phase();
     my $end5   = $location->end5();
     my $end3   = $location->end3();
-
-    # List of just getters
     my $length = $location->length();
 
-See JCVI::Range::Interface for more documentation.
+See JCVI::Range for more documentation.
 
+=head2 DESCRIPTION
+
+Location is a source, a range and a phase. Locations can be implemented using
+any constructor present in the JCVI::Range class; the parameters to that
+constructor are passed as an arrayref as the second parameter to location's
+constructor. E.g.
+
+    my $range    = JCVI::Range->new_53( $end5, $end3 ); # Range's constructor
+    my $location = JCVI::Location->new_53( $source, [ $end5, $end3 ], $phase );
+
+And of JCVI::Range's methods can be accessed directly through location:
+
+    $range->extend( ... );
+    $location->extend( ... );
+
+These methods are automatically generated the first time they are called.
 =cut
-
-my $SOURCE_INDEX = 0;
-my $LOWER_INDEX  = 1;
-my $LENGTH_INDEX = 2;
-my $STRAND_INDEX = 3;
-my $PHASE_INDEX  = 4;
-
-our $NON_NEG_INT_REGEX = qr/^\d+$/;
-our $POS_INT_REGEX     = qr/^[1-9]\d*$/;
-our $STRAND_REGEX      = qr/^[+-]?[01]$/;
-our $PHASE_REGEX       = qr/^[0-2]$/;
-
-=head1 CONSTRUCTORS
-
-=cut
-
-=head2 new_53
-
-=cut
-
-sub new_53 {
-    my $class = shift;
-    my ( $source, $e5, $e3, $phase ) = validate_pos(
-        @_, 1,
-        ( { regex => $POS_INT_REGEX } ) x 2,
-        { default => 0, regex => $PHASE_REGEX }
-    );
-
-    my $lower = min( $e5, $e3 ) - 1;
-    my $length = max( $e5, $e3 ) - $lower;
-    my $strand = ( $e3 <=> $e5 ) || undef;
-
-    return bless( [ $source, $lower, $length, $strand, $phase ], $class );
-}
 
 =head1 ACCESSORS
 
 =cut
 
+my $SOURCE_INDEX = 0;
+my $RANGE_INDEX  = 1;
+my $PHASE_INDEX  = 2;
+
+our $PHASE_REGEX = qr/^\+?[0-2](?:\.0*)$/;
+
 =head2 source
 
-    $source = $location->source();
-    $location->source($source);
-
-Get/set the source
+    my $source = $location->source();
+    $location->source( $source );
 
 =cut
 
 sub source {
-    my $self = shift;
-    return $self->[$SOURCE_INDEX] unless (@_);
-    return $self->[$SOURCE_INDEX] = $_[0];
+    return $_[0][$SOURCE_INDEX] unless ( @_ > 1 );
+    return $_[0][$SOURCE_INDEX] = $_[1];
 }
 
-=head2 lower
+=head2 range
 
-    $lower = $location->lower();
-    $location->lower($lower); 
-
-Get/set the lower bound.
+    my $range = $location->range();
+    $location->range( $range );
 
 =cut
 
-sub lower {
-    my $self = shift;
-    return $self->[$LOWER_INDEX] unless (@_);
-
-    # Validate the lower bound
-    croak 'Lower bound must be a non-negative integer'
-      unless ( $_[0] =~ /$NON_NEG_INT_REGEX/ );
-
-    # Adjust the length and lower bound
-    $self->_set_length( $self->upper() - $_[0] );
-    return $self->[$LOWER_INDEX] = $_[0];
-}
-
-=head2 upper
-
-    $upper = $location->upper();
-    $location->upper($upper); 
-
-Get/set the upper bound.
-
-=cut
-
-sub upper {
-    my $self = shift;
-
-    # upper = lower + length
-    return $self->lower() + $self->length() unless (@_);
-
-    # new_upper = lower + new_set_length
-    # new_set_length = new_upper - lower
-    $self->_set_length( $_[0] - $self->lower );
-    return $_[0];
-}
-
-=head2 length
-
-    $length = $location->length();
-
-Get the length.
-
-=cut
-
-sub length { return $_[0][$LENGTH_INDEX] }
-
-# Set the length. The lower bound is the anchor (upper bound changes).
-sub _set_length {
-    my $self = shift;
-
-    # Validate the length
-    croak 'Length must be a non-negative integer'
-      unless ( $_[0] =~ /$NON_NEG_INT_REGEX/ );
-
-    return $self->[$LENGTH_INDEX] = $_[0] * 1;
-}
-
-=head2 strand
-
-    $strand = $location->strand();
-    $location->strand($strand);
-
-Get/set the strand. Strand may be undef, 0, 1, or -1. Here are the meanings of
-the four values:
-
-    1   - "+" strand
-    -1  - "-" strand
-    0   - strandless
-    undef - unknown
-
-=cut
-
-sub strand {
-    my $self = shift;
-
-    return $self->[$STRAND_INDEX] unless (@_);
-
-    # Delete strand if undef passed
-    return undef( $self->[$STRAND_INDEX] ) unless ( defined $_[0] );
-
-    # Validate strand
-    croak 'Value passed to strand must be undef, 0, 1, or -1'
-      unless ( $_[0] =~ /$STRAND_REGEX/ );
-    return $self->[$STRAND_INDEX] = $_[0] * 1;
+sub range {
+    return $_[0][$RANGE_INDEX] unless ( @_ > 1 );
+    return $_[0][$RANGE_INDEX] = $_[1];
 }
 
 =head2 phase
 
     my $phase = $location->phase();
-    $location->phase( $new_phase );
-
-Get/set the phase. Phase may be 0, 1 or 2. The phase is the offset of the next
-frame of the coding region from the start of the location.
+    $location->phase( $phase );
 
 =cut
 
 sub phase {
+    return $_[0][$PHASE_INDEX] unless ( @_ > 1 );
+    croak "Invalid phase $_[1] (must be one of 0, 1 or 2)"
+      unless ( $_[1] =~ $PHASE_REGEX );
+    return $_[0][$SOURCE_INDEX] = $_[1] * 1;
+}
+
+our $AUTOLOAD;
+
+# "Magic" code that autogenerates the JCVI::Range methods
+sub AUTOLOAD {
     my $self = shift;
+    my ($sub_name) = $AUTOLOAD =~ m/(\w+)$/;
 
-    return $self->[$PHASE_INDEX] unless (@_);
+    my $class;
+    my $sub;
+    
+    # Create wrapper around a JCVI::Range object method
+    if ( $class = ref($self) ) {
+        croak qq{Invalid method "$sub_name"}
+          unless ( JCVI::Range->can($sub_name) );
+        
+        $sub = sub {
+            return shift->range->$sub_name(@_);
+        };
+    }
+    # Create wrapper around a JCVI::Range constructor
+    else {
+        $class = $self;
 
-    # Validate the phase
-    croak 'Value passed to phase must be 0, 1 or 2'
-      unless ( $_[0] =~ /$PHASE_REGEX/ );
-    return $self->[$PHASE_REGEX] = $_[0];
+        croak qq{Invalid constructor "$sub_name"}
+          unless ( JCVI::Range->can($sub_name) );
+
+        $sub = sub {
+            my $class = shift;
+            my ( $source, $range_params, $phase ) = @_;
+
+            # Validate phase and create the range object
+            if ($phase) {
+                croak "Invalid phase $phase (must be one of 0, 1 or 2)"
+                  unless ( $_[1] =~ $PHASE_REGEX );
+            }
+            my $range = JCVI::Range->$sub_name(@$range_params);
+
+            # Create the location object
+            return bless [ $source, $range, $phase ], $class;
+        };
+    }
+
+    # Inject the method and run it
+    no strict 'refs';
+    *{"${class}::${sub_name}"} = \&$sub;
+    $self->$sub_name(@_);
 }
 
 =head1 AUTHOR
