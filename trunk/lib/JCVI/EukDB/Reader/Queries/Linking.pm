3d3a0974a4c2f4e24851313a7e3edb9e2e920317
kgalinsk <kgalinsk@061605af-e407-0410-a945-9c8a22772c06>
2010-01-05 19:54:44 +0000

    Added documentation for query-implementing modules.

--- trunk/lib/JCVI/EukDB/Reader/Queries/Linking.pm
+++ trunk/lib/JCVI/EukDB/Reader/Queries/Linking.pm
@@ -1,7 +1,40 @@
+# File: Linking.pm
+# Author: kgalinsk
+# Created: Dec 31, 2009
+#
+# $Author$
+# $Date$
+# $Revision$
+# $HeadURL$
+#
+# Copyright 2009, J. Craig Venter Institute
+#
+# JCVI::EukDB::Reader::Linking - link parent/children features
+
 package JCVI::EukDB::Reader::Queries::Linking;
 
 use strict;
 use warnings;
+
+=head1 NAME
+
+JCVI::EukDB::Reader::Linking - link parent/children features
+
+=head1 SYNOPSIS
+
+    my $temp2 = $dao->feat_names_temp_table_to_parents_temp_table( $temp1 );
+    my $temp2 = $dao->feat_names_tt2parents_tt( $temp1 );
+
+    my $temp = $dao->feat_names_arrayref_to_parents_temp_table( $feat_names );
+    my $temp = $dao->feat_names_arrayref2parents_tt( $feat_names );
+    
+    my $temp2 = $dao->feat_names_temp_table_to_children_temp_table( $temp1 );
+    my $temp2 = $dao->feat_names_tt2children_tt( $temp1 );
+
+    my $temp = $dao->feat_names_arrayref_to_children_temp_table( $feat_names );
+    my $temp = $dao->feat_names_arrayref2children_tt( $feat_names );
+
+=cut
 
 use JCVI::EukDB::Reader::Queries [
     { name  => 'feat_name',   as     => 'child' },
